ae21f5f0e66ee678424ef304065a61406f7a8c24
JacekMielanczuk <mielanczuk1988@gmail.com>
2023-02-27 20:06:06 +0100

    zadanie 1
    zadanie 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+import '../css/common.css';
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+stopBtn.disabled = true;
+let timeId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timeId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timeId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
