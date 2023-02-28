3e2715ab279177e7666ca52c194a5619d95059d4
JacekMielanczuk <mielanczuk1988@gmail.com>
2023-02-28 19:23:34 +0100

    podpięcie elementów do js
    podpięcie elementów do js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,12 @@ import '../css/common.css';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const startBtn = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+let timerId = null;
+
+startBtn.setAttribute('disabled', true);
